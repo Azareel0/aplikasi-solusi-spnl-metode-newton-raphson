7587f79ee7573d8eac395c492f2df1e47837f64e
Azareel0 <azareeljs404@gmail.com>
2025-11-30 14:38:15 +0700

    Menambahkan input untuk tebakan awal dari user

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,9 @@ st.title("Aplikasi SPNL Newton Raphson")
 st.write("Aplikasi ini digunakan untuk menyelesaikan Sistem Persamaan Non-Linear dengan menggunakan metode Newton Raphson")
 
 st.subheader("Masukkan Persamaan")
-f1 = st.text_input("Persamaan 1", placeholder = "x**2 + y**2 - 5")
-f2 = st.text_input("Persamaan 2", placeholder = "x*y - 1")
+f1 = st.text_input("Persamaan 1", placeholder = "contoh = x**2 + y**2 - 5")
+f2 = st.text_input("Persamaan 2", placeholder = "contoh = x*y - 1")
+
+st.subheader("Masukkan Tebakan Awal")
+x0 = st.number_input("Tebakan awal x", value=1.0)
+y0 = st.number_input("Tebakan awal y", value=1.0)
