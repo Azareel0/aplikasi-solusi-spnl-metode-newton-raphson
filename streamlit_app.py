c864ae4a39f1217e41743b5993cd7912921e479a
Azareel0 <azareeljs404@gmail.com>
2025-11-30 14:24:45 +0700

    Edit value menjadi placeholder pada input persamaan

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title("Aplikasi SPNL Newton Raphson")
 st.write("Aplikasi ini digunakan untuk menyelesaikan Sistem Persamaan Non-Linear dengan menggunakan metode Newton Raphson")
 
-st.write("Masukkan Persamaan")
-f1 = st.text_input("Persamaan 1", value = "x**2 + y**2 - 5")
-f2 = st.text_input("Persamaan 2", value = "x*y - 1")
+st.subheader("Masukkan Persamaan")
+f1 = st.text_input("Persamaan 1", placeholder = "x**2 + y**2 - 5")
+f2 = st.text_input("Persamaan 2", placeholder = "x*y - 1")
