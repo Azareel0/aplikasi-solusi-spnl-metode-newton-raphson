f1dfeccef9c5b3af70715a3961d24b95bb6ab1fe
Azareel0 <azareeljs404@gmail.com>
2025-11-30 14:15:55 +0700

    Memberikan input untuk persamaan dari user

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 st.title("Aplikasi SPNL Newton Raphson")
 st.write("Aplikasi ini digunakan untuk menyelesaikan Sistem Persamaan Non-Linear dengan menggunakan metode Newton Raphson")
+
+st.write("Masukkan Persamaan")
+f1 = st.text_input("Persamaan 1", value = "x**2 + y**2 - 5")
+f2 = st.text_input("Persamaan 2", value = "x*y - 1")
