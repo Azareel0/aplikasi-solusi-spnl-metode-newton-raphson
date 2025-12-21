2036869b9366dd02067b400964bd8aeed93fc75f
Azareel0 <azareeljs404@gmail.com>
2025-12-21 12:32:19 +0700

    Menampilkan input persamaan dan tebakan awal dari user pada tombol hitung

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,5 +16,6 @@ tol = st.number_input("Toleransi", value = 1e-6, min_value = 0.0, format = "%.6f
 max_iter = st.number_input("Maksimum iterasi", value = 50, min_value = 1, max_value = 1000)
 
 if st.button("Hitung"):
-  hasil = x0 + y0
-  st.write("Hasil: ", hasil)
+  st.write("Persamaan 1: ", f1)
+  st.write("Persamaan 2: ", f2)
+  st.write("Tebakan awal: x = ", x0, ", y = ", y0)
