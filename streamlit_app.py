abcdc68662733e0dc5e47b4b84da8f5e76160e23
Azareel0 <azareeljs404@gmail.com>
2025-12-29 11:14:02 +0700

    Coba cek ulang untuk menampilkan fungsi numerik

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,8 @@ if st.button("Hitung Solusi"):
         F_func = sp.lambdify((x, y), F, "numpy")
         J_func = sp.lambdify((x, y), J, "numpy")
 
+        F_val = np.array(F_func(x0, y0), dtype=float).flatten()
+        J_val = np.array(J_func(x0, y0), dtype=float)
         st.write("F(x0, y0) =", F_val)
         st.write("J(x0, y0) =", J_val)
     except:
