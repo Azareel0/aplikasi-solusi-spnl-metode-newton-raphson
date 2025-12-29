4e397d40999bc89cbd15944242e34d489458c268
Azareel0 <azareeljs404@gmail.com>
2025-12-29 13:43:26 +0700

    Mengganti max value pada inputan max iterasi

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ y0 = st.number_input("Tebakan awal y", value=1.0)
 
 st.subheader("Masukkan Parameter")
 tol = st.number_input("Toleransi", value = 1e-6, min_value = 0.0, format = "%.6e")
-max_iter = st.number_input("Maksimum iterasi", value = 50, min_value = 1, max_value = 1000)
+max_iter = st.number_input("Maksimum iterasi", value = 50, min_value = 1, max_value = 50)
 
 if st.button("Hitung Solusi"):
     try:
