dc497e395abd8e7432a3b63d7fb9a47c43a07717
Azareel0 <azareeljs404@gmail.com>
2026-01-02 12:55:48 +0700

    Mengimport library pandas pada code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import sympy as sp
 import numpy as np
+import pandas as pd
 
 st.title("Aplikasi SPNL Newton Raphson")
 st.write("Aplikasi ini digunakan untuk menyelesaikan Sistem Persamaan Non-Linear dengan menggunakan metode Newton Raphson")
